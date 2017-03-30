24959906ccf7e6dd281457eef484f0b7f12b6761
hawkchen <hawkchen@potix.com>
2017-03-30 17:34:46 +0800

    initial import. add reCAPTCHA examples

---
+++ src/main/java/org/zkoss/support/smalltalk/recaptcha/RecaptchaComposer.java
@@ -0,0 +1,33 @@
+package org.zkoss.support.smalltalk.recaptcha;
+
+import org.zkoss.json.JSONObject;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.*;
+import org.zkoss.zk.ui.util.Clients;
+import org.zkoss.zul.Button;
+
+public class RecaptchaComposer extends SelectorComposer<Component> {
+
+	@Wire
+	Button submit;
+	
+	final String SECRET = "6Lcj5AYTAAAAANcaQYWvFkHVSkqR6FsBaCXXw54r"; //from reCaptcha
+	
+	@Listen("onUserRespond = #recaptcha")
+	public void verify(Event event) throws Exception{
+		JSONObject result = RecaptchaVerifier.verifyResponse(SECRET, ((JSONObject)event.getData()).get("response").toString());
+		if (Boolean.parseBoolean(result.get("success").toString())){
+			submit.setDisabled(false);
+		}else{
+			String errorCode = result.get("error-codes").toString();
+			//log or show error
+		}
+	}
+	
+	@Listen("onClick = #submit")
+	public void submit(){
+		Clients.showNotification("submitted");
+	}
+}
---
+++ src/main/java/org/zkoss/support/smalltalk/recaptcha/RecaptchaVM.java
@@ -0,0 +1,39 @@
+package org.zkoss.support.smalltalk.recaptcha;
+
+import org.zkoss.bind.annotation.*;
+import org.zkoss.json.JSONObject;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.util.Clients;
+
+@ToServerCommand("verify")
+public class RecaptchaVM extends SelectorComposer<Component> {
+
+	private boolean disabled = true;
+	
+	final String SECRET = "6Lcj5AYTAAAAANcaQYWvFkHVSkqR6FsBaCXXw54r"; //from reCaptcha
+	
+	@Command @NotifyChange("disabled")
+	public void verify(@BindingParam("response")String response) throws Exception{
+		JSONObject result = RecaptchaVerifier.verifyResponse(SECRET, response);
+		if (Boolean.parseBoolean(result.get("success").toString())){
+			disabled = false;
+		}else{
+			String errorCode = result.get("error-codes").toString();
+			//log or show error
+		}
+	}
+	
+	@Command
+	public void submit(){
+		Clients.showNotification("submitted");
+	}
+
+	public boolean isDisabled() {
+		return disabled;
+	}
+
+	public void setDisabled(boolean disabled) {
+		this.disabled = disabled;
+	}
+}
